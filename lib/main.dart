44889e7e408b0744bd3dc1ef28b767df8359a2eb
DESKTOP-AGAU61U\SEP <marlysalem348@gmail.com>
2026-04-21 10:18:08 +0300

    initial project for rafiq app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return (runApp(const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+        body: const Center(child: Text('Hello, World!')),
+      ),
+    );
+  }
+}
